f5f0344501c037ef86adc8120b455976bd169245
creat73 <nikill1125@gmail.com>
2017-11-05 13:37:07 +0100

    Add main menu options

---
+++ main.py
@@ -0,0 +1,33 @@
+def main():
+    menu_options = ['Exit', 'Add new item', 'Delete item', "Modify item's description", "Modify item's name",
+                    'Mark item as done', 'Show items', 'Show specific item']
+    
+    if option == '1':
+        pass
+
+    elif option == '2':
+        pass
+
+    elif option == '3':
+        pass
+
+    elif option == '4':
+        pass
+
+    elif option == '5':
+        pass
+
+    elif option == '6':
+        pass
+
+    elif option == '7':
+        pass
+
+    elif option == '0':
+        pass
+
+    else:
+        pass
+
+if __name__ == '__main__':
+    main()
